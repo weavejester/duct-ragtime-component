17c084b7d2a36ffb4c6e4b592eef8d732b4dbf07
James Reeves <jreeves@weavejester.com>
2015-08-06 19:54:42 +0100

    Add docstrings

--- src/duct/component/ragtime.clj
+++ src/duct/component/ragtime.clj
@@ -1,4 +1,5 @@
 (ns duct.component.ragtime
+  "A component for handling migrations on a SQL database."
   (:require [com.stuartsierra.component :as component]
             [ragtime.core :as core]
             [ragtime.jdbc :as jdbc]
@@ -18,14 +19,31 @@
   {:strategy strategy/raise-error
    :reporter repl/default-reporter})
 
-(defn ragtime [options]
+(defn ragtime
+  "Create a Ragtime component for handling migrations. Expects a dependency :db
+  that has a key :spec containing a clojure.java.jdbc compatible db-spec map.
+
+  Takes the following options:
+
+    :resource-path - the resource path to find migration files
+    :strategy      - the Ragtime strategy
+                     (defaults to ragtime.strategy/raise-error)
+    :reporter      - the reporter function
+                     (defaults to ragtime.repl/default-reporter)"
+  [options]
+  {:pre [(contains? options :resource-path)]}
   (map->Ragtime (merge default-options options)))
 
-(defn migrate [{:keys [datastore migrations strategy reporter]}]
+(defn migrate
+  "Migrates the dependent database to the latest migration."
+  [{:keys [datastore migrations strategy reporter]}]
   (let [migrations (map #(repl/wrap-reporting % reporter) migrations)]
     (core/migrate-all datastore {} migrations strategy)))
 
 (defn rollback
+  "Rolls the dependent database back to a specific migration ID, or by a fixed
+  number of migrations. If supplied with only one argument, the database is
+  rolled back by only one migration."
   ([component]
    (rollback component 1))
   ([{:keys [datastore migrations reporter]} amount-or-id]
