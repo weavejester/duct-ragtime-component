4cd3d029321ce105a37cbebb93a2bf7268664de3
James Reeves <jreeves@weavejester.com>
2015-08-06 18:11:26 +0100

    Make reporter function an option

--- src/duct/component/ragtime.clj
+++ src/duct/component/ragtime.clj
@@ -14,16 +14,17 @@
   (stop [component]
     (dissoc component :datastore :migrations)))
 
-(defn ragtime [options]
-  (map->Ragtime options))
+(def default-options
+  {:strategy strategy/raise-error
+   :reporter repl/default-reporter})
 
-(defn migrate
-  [{:keys [datastore migrations strategy]
-    :or {strategy strategy/raise-error}}]
-  (let [migrations (map #(repl/wrap-reporting % repl/default-reporter) migrations)]
+(defn ragtime [options]
+  (map->Ragtime (merge default-options options)))
+
+(defn migrate [{:keys [datastore migrations strategy reporter]}]
+  (let [migrations (map #(repl/wrap-reporting % reporter) migrations)]
     (core/migrate-all datastore {} migrations strategy)))
 
-(defn rollback
-  [{:keys [datastore migrations]}]
-  (let [migrations (map #(repl/wrap-reporting % repl/default-reporter) migrations)]
+(defn rollback [{:keys [datastore migrations reporter]}]
+  (let [migrations (map #(repl/wrap-reporting % reporter) migrations)]
     (core/rollback-last datastore (core/into-index migrations) 1)))
