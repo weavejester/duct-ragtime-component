3d41455ce82e86956580f157b5570e1a553cce40
James Reeves <jreeves@weavejester.com>
2015-08-06 01:17:29 +0100

    Initial commit

---
+++ test/duct/component/ragtime_test.clj
@@ -0,0 +1,7 @@
+(ns duct.component.ragtime-test
+  (:require [clojure.test :refer :all]
+            [duct.component.ragtime :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
